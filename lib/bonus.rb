fd626fe0828c10c4815ae02962e1aa99bc95bbbd
Ethan Roberts <ejrobe@gmail.com>
2019-01-08 20:49:42 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  puts epic_tragedy[:hero].[:status]
+  
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
